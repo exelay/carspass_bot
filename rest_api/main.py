33f91c91cc83f458a1db74d79cfbd107f8e38c89
Alexey <exelay.rapik@gmail.com>
2021-02-02 19:10:35 +0300

    Updated fastapi main

--- rest_api/main.py
+++ rest_api/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from routers import notification
@@ -9,11 +10,13 @@ tags_metadata = [
         "description": "Notify user about new ad.",
     }
 ]
-
 app = FastAPI(
     title="Spider API for CarsPass Telegram Bot",
     version="0.0.1",
     openapi_tags=tags_metadata,
 )
-
 app.include_router(notification.router)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
