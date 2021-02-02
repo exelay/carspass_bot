db4ef4d120fbb2ae8fc715f9b334e23b96bd6f59
Alexey <exelay.rapik@gmail.com>
2021-02-02 16:51:24 +0300

    Rewrote reply keyboard

--- handlers/basic/start.py
+++ handlers/basic/start.py
@@ -7,9 +7,11 @@ from messages import MESSAGES
 
 @dp.message_handler(CommandStart())
 async def start_command(message: types.Message):
-    markup = types.ReplyKeyboardMarkup().add(
-        types.KeyboardButton("🚨Авторизоваться🚨", request_contact=True)
-    )
+    markup = types.ReplyKeyboardMarkup([
+        [
+            types.KeyboardButton("🚨Авторизоваться🚨", request_contact=True)
+        ]
+    ], one_time_keyboard=True)
     user_name = message.from_user.first_name
     text = MESSAGES['hello'].format(user_name)
     await message.answer(text, reply_markup=markup)
