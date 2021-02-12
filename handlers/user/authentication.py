838358cadd51cae05fb4bf4a54cc11d6577d177e
Alexey <exelay.rapik@gmail.com>
2021-02-12 16:50:45 +0300

    Added notify about new user.

--- handlers/user/authentication.py
+++ handlers/user/authentication.py
@@ -1,27 +1,34 @@
 import json
 import sqlite3
 import requests
+from loguru import logger
 from aiogram import types
 
 from loader import dp, db
+from utils.notify_admins import new_user_notify, error_notify
 from messages import MESSAGES
 
 
 @dp.message_handler(content_types=types.ContentTypes.CONTACT)
 async def auth_handler(message: types.Message):
-    id_ = message.from_user.id
-    phone = message.contact.phone_number
-    response = requests.get(
-        "https://app.carspass.ru/webhook/telegram",
-        params={"phone": phone}
-    )
-    response_json = json.loads(response.text)
-    if response.status_code == 200:
-        code = response_json['code']
-        try:
-            db.add_user(id_, phone, code)
-        except sqlite3.IntegrityError:
-            pass
-        await message.answer(MESSAGES['auth_success'].format(code))
-    else:
-        await message.answer(MESSAGES['auth_fail'])
+    try:
+        id_ = message.from_user.id
+        phone = message.contact.phone_number
+        response = requests.get(
+            "https://app.carspass.ru/webhook/telegram",
+            params={"phone": phone}
+        )
+        response_json = json.loads(response.text)
+        if response.status_code == 200:
+            code = response_json['code']
+            try:
+                db.add_user(id_, phone, code)
+                await new_user_notify(dp, message)
+            except sqlite3.IntegrityError:
+                pass
+            await message.answer(MESSAGES['auth_success'].format(code))
+        else:
+            await message.answer(MESSAGES['auth_fail'])
+    except Exception as err:
+        await error_notify(dp, err)
+        logger.error(f"Something went wrong: {err}")
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,25 +1,37 @@
 from loguru import logger
-from aiogram import Dispatcher
+from aiogram import Dispatcher, types
 
 from data.config import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher, db):
-    message = "Бот запущен! Всего пользователей: {}"
+    text = "Бот запущен! Всего пользователей: {}"
 
     for admin in ADMINS:
         try:
             count = db.count_users()[0]
-            await dp.bot.send_message(admin, message.format(count))
+            await dp.bot.send_message(admin, text.format(count))
         except Exception as err:
             logger.error(f"Message is not delivered. Exception: {err}")
 
 
 async def error_notify(dp: Dispatcher, error):
-    message = f"Что-то пошло не так! Ошибка: {error}"
+    text = f"Что-то пошло не так! Ошибка: {error}"
 
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, message)
+            await dp.bot.send_message(admin, text)
+        except Exception as err:
+            logger.error(f"Message is not delivered. Exception: {err}")
+
+
+async def new_user_notify(dp: Dispatcher, message: types.Message):
+    name = message.from_user.first_name
+    phone = message.contact.phone_number
+    text = f"Авторизовался новый пользователь:\n{name} | {phone}"
+
+    for admin in ADMINS:
+        try:
+            await dp.bot.send_message(admin, text)
         except Exception as err:
             logger.error(f"Message is not delivered. Exception: {err}")
