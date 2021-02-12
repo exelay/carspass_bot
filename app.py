8fb29d10520047a314a45d0adf582d2e90501620
Alexey <exelay.rapik@gmail.com>
2021-02-12 14:55:24 +0300

    Logged up starting bot function

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from loguru import logger
 from loader import db
 
 
 async def on_startup(dp):
+    logger.add('debug.log', level='DEBUG', rotation='10 KB', compression='zip')
     try:
+        logger.info('Creating tables...')
         db.create_table_users()
     except Exception as err:
-        print(err)
+        logger.error(f'Table creation error: {err}')
+    else:
+        logger.info('Table successfully created!')
 
 
 if __name__ == '__main__':
